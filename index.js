fd7fd12ac4a8207cc829865eb656cdff157d1bda
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-03-15 11:09:05 -0700

    added error handling

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const pyProg = spawn('python3', ['-c', 'import sys; print(2 + 2)']);
 
 function myFunc() {
   pyProg.stdout.on('data', function(data) {
-    console.log(data.toString());
+    process.stdout.write(data.toString());
+});
+  pyProg.stderr.on('data', (data) => {
+    console.error(`Python3 Error: ${data}`);
+  });
+  pyProg.on('close', (code) => {
+    if (code !== 0) {
+        console.log(`Python3 process exited with code ${code}`);
+    }
 });
 }
 myFunc()
